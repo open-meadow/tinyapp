1486e63f57f03bc578c4e98beb69c3d2d05ca351
open-meadow <rohan.varughese@protonmail.com>
2022-11-28 22:21:41 +0000

    Add Bootstrap and JQuery

--- express_server.js
+++ express_server.js
@@ -25,3 +25,7 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b><body><html>")
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars)
+})
