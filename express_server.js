6daa0ec7d48c93b2c111dde93f4ce18842a4ea2b
open-meadow <rohan.varughese@protonmail.com>
2022-11-29 00:42:56 +0000

    Add url-index.ejs and header.ejs

--- express_server.js
+++ express_server.js
@@ -28,4 +28,9 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
-})
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+});
