e6036fc9f268e1481a8f4a078f455c6db199595f
open-meadow <rohan.varughese@protonmail.com>
2022-11-30 20:10:02 +0000

    Implement new user registration

--- express_server.js
+++ express_server.js
@@ -42,6 +42,15 @@ const users = {
   },
 };
 
+// Function that checks user database to see if the same email has been input
+const checkUserDatabase = (newEmail) => {
+  for (i in users) {
+    console.log(i);
+  }
+}
+
+checkUserDatabase();
+
 // Test. Delete later
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -64,21 +73,25 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b><body><html>");
 });
 
-// render urls_index page
+// render urls_index page. can refresh here to retest
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
+  
+  const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]] };
+
   res.render("urls_index", templateVars);
 });
 
 // this section allows the user to input a new URL
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies['username'] }
+  const templateVars = { user: users[req.cookies["user_id"]] };
+  
   res.render("urls_new", templateVars);
 });
 
 // this section shows the URL details page
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies['username']};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies["user_id"]]};
+  
   res.render("urls_show", templateVars);
 });
 
@@ -87,9 +100,12 @@ app.post("/urls", (req, res) => {
   console.log(req.body);
   let newUrl = getRandomString();
   urlDatabase[newUrl] = req.body.longURL;
-  // res.send("Ok");
+  
   // Redirect to URL Index with new Url
-  const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
+  const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]]};
+
+  // add new user
+  console.log(users[req.cookies["user_id"]]);
   res.render("urls_index", templateVars);
 });
 
@@ -116,19 +132,28 @@ app.post("/urls/:id/longURL", (req, res) => {
 
 // this section lets the user log in
 app.post("/login", (req, res) => {
-  console.log(req.body.username);
-  res.cookie("username", req.body.username);
+  // console.log(req.body.username);
+  // res.cookie("username", req.body.username);
   res.redirect("/urls");
 });
 
 // this section lets the user log out and clears all cookies
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
 // this section leads to the registration page
 app.get("/register", (req, res) => {
-  const templateVars = { username: "" };
+  const templateVars = { user: null };
   res.render("url_register", templateVars);
+});
+
+// this section adds a new user to the Users database
+app.post("/register", (req, res) => {
+  let newRandomId = getRandomString();
+  users[newRandomId] = { id: newRandomId, email: req.body.email, password: req.body.password};
+  console.log(users);
+  res.cookie("user_id", newRandomId);
+  res.redirect("/urls");
 });
