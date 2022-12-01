75ced0f91dfa0dc19c321837a97556ab37787e6c
open-meadow <rohan.varughese@protonmail.com>
2022-12-01 00:44:36 +0000

    Add basic permission features

--- express_server.js
+++ express_server.js
@@ -88,6 +88,12 @@ app.get("/urls", (req, res) => {
 
 // this section allows the user to input a new URL
 app.get("/urls/new", (req, res) => {
+
+  // if user is not logged in, redirect them to login
+  if(!req.cookies["urls_id"]) {
+    res.redirect("/login");
+  }
+
   const templateVars = { user: users[req.cookies["user_id"]] };
   
   res.render("urls_new", templateVars);
@@ -95,6 +101,14 @@ app.get("/urls/new", (req, res) => {
 
 // this section shows the URL details page
 app.get("/urls/:id", (req, res) => {
+
+  const checkID = Object.keys(urlDatabase).includes(req.params.id);
+  console.log("Value of checkID is ", checkID);
+
+  if(!checkID) {
+    res.send("<html>This ID Does Not Exist.</html>");
+  }
+
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies["user_id"]]};
   
   res.render("urls_show", templateVars);
@@ -103,6 +117,12 @@ app.get("/urls/:id", (req, res) => {
 // This section creates a new URL and adds it to the index
 app.post("/urls", (req, res) => {
   // console.log(req.body);
+
+  if (!req.cookies["urls_id"]) {
+    res.send("<html><body>You cannot shorten URL's because you are not logged in</body></html>");
+    return;
+  }
+
   let newUrl = getRandomString();
   urlDatabase[newUrl] = req.body.longURL;
   
@@ -110,19 +130,27 @@ app.post("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]]};
 
   // add new user
-  console.log(users[req.cookies["user_id"]]);
+  // console.log(users[req.cookies["user_id"]]);
   res.render("urls_index", templateVars);
 });
 
 // this section redirects fron the path to the longURL
 app.get("/u/:id", (req, res) => {
+
+  const checkID = Object.keys(urlDatabase).includes(req.params.id);
+  console.log("Value of checkID is ", checkID);
+
+  if(!checkID) {
+    res.send("<html>This ID Does Not Exist.</html>")
+  }
+
   const longUrl = urlDatabase[req.params.id];
   res.redirect(longUrl);
 });
 
 // this section deletes an id and URL
 app.post("/urls/:id/delete", (req, res) => {
-  console.log(req.params);
+  // console.log(req.params);
   delete urlDatabase[req.params.id];
   // const templateVars = { urls: urlDatabase };
   res.redirect("/urls");
@@ -130,13 +158,20 @@ app.post("/urls/:id/delete", (req, res) => {
 
 // this section updates the long URL of an ID
 app.post("/urls/:id/longURL", (req, res) => {
-  console.log(req.body.longURL);
+  // console.log(req.body.longURL);
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls");
 });
 
 // this section leads the user to the login page
 app.get("/login", (req, res) => {
+
+  // if user is already logged in, redirect to /urls
+  // console.log("Check user ID cookie", req.cookies["user_id"]);
+  if(req.cookies["user_id"]) {
+    res.redirect("/urls");
+  }
+
   const templateVars = { user: null };
   res.render("urls_login", templateVars);
 });
@@ -168,6 +203,13 @@ app.post("/logout", (req, res) => {
 
 // this section leads to the registration page
 app.get("/register", (req, res) => {
+
+  // if user is already logged in, redirect to /urls
+  // console.log("Check user ID cookie", req.cookies["user_id"]);
+  if(req.cookies["user_id"]) {
+    res.redirect("/urls");
+  }
+
   const templateVars = { user: null };
   res.render("url_register", templateVars);
 });
